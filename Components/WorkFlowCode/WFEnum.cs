40f3022efdeb383a18ae53dfeb1cae6a9b4ac077
gangbeng <gangbeng@xiaokakeji.com>
2016-06-01 16:59:18 +0800

    init branch

---
+++ Components/WorkFlowCode/WFEnum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Components.WorkFlowCode
+{
+    /// <summary>
+    /// 流程状态
+    /// </summary>
+    public enum WF_ActiveState
+    {
+
+    }
+
+    /// <summary>
+    /// 节点状态
+    /// </summary>
+    public enum WF_NodeState
+    {
+
+    }
+}
